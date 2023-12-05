89683239178691dcb5d44bd9f2ed2bf649f33406
pavlos <pavloskoz@hotmail.com>
2023-12-05 09:05:34 +0100

    commit base uri check

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import SynonymWordTabs from './components/synonym-word-tabs-component';
+import { BASE_URI } from './config';
 
 function App() {
+  console.log(BASE_URI)
   return (
     <div className="h-screen flex items-center justify-center">
       <SynonymWordTabs />
